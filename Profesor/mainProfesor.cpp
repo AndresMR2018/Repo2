deed6579f869738056baecad451942bbb56c5a4d
Cristian Muyudumbay <cristian.muyudumbay@gmail.com>
2018-07-23 09:07:17 -0500

    MAINPROFESOR

---
+++ Profesor/mainProfesor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Profesor2.h"
+#include "Contratado.h"
+#include "Titular.h"
+#include "Dictado.h"
+#include "Materia.h"
+
+int main(){
+	system("color a");
+	Contratado objC;
+	objC.ingresar();
+	objC.imprimir();
+	Titular objT;
+	objT.ingresar();
+	Dictado objD;
+	objD.ingresar();
+	objD.imprimir();
+	Materia objM;
+	objM.ingresar();
+	objM.imprimirt();
+	return 0;
+}
